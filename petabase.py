a7304549e0db50267165034ecb8ef5770d5792ac
Colin Maudry <colin@maudry.com>
2021-10-27 14:13:24 +0200

    Many improvements

--- petabase.py
+++ petabase.py
@@ -1,24 +1,26 @@
 import argparse
 from metabase_api import Metabase_API
-import sys
 import os
 
 mb_url = os.getenv('METABASE_URL')
 mb_user = os.getenv('METABASE_USER')
 mb_password = os.getenv('METABASE_PASSWORD')
 mbapi = Metabase_API(mb_url, mb_user, mb_password)
+options = {}
 
-def main(args):
-    options = parseArguments(args)
+
+def main():
+    global options
+    options = parseArguments()
     if options['setNames']:
-        setNames(options)
+        setNames()
     elif options['clone']:
-        clone(options)
+        clone()
     else:
         print('What do you want?')
 
 
-def setNames(options):
+def setNames():
     import re
 
     collection = mbapi.get('/api/collection/{}'.format(options['setNames']))
@@ -34,21 +36,30 @@ def setNames(options):
             mbapi.put('/api/card/{}'.format(item['id']), json=item)
 
 
-def clone(options):
-    mbapi = Metabase_API(mb_url, mb_user, mb_password)
-    mbapi.copy_collection(source_collection_id=options['clone'][0], destination_parent_collection_id=options['clone'][1])
+def clone():
+    sourceCollection = options['clone'][0]
+    parentCollection = options['clone'][1]
 
-    if options['bsdType']:
-        sourceCollectionName = mbapi.get('/api/collection/{}'.format(options['clone'][0]))['name']
-        newCollectionId = getCollectionId(options['clone'][1], sourceCollectionName)
-        newCollection = mbapi.get('/api/collection/{}'.format(newCollectionId))
-        newCollection['name'] = options['bsdType']
-        mbapi.put('/api/collection/{}'.format(newCollectionId), json=newCollection)
-        setNamesOptions = {
-            'setNames': newCollectionId
-        }
-        setNames(setNamesOptions)
+    # Equal ids lead to infinite creation of clones
+    assert sourceCollection != parentCollection
 
+    mbapi.copy_collection(source_collection_id=sourceCollection,
+                          destination_parent_collection_id=parentCollection)
+
+    if options['bsdType'] or options['database']:
+        sourceCollectionName = mbapi.get('/api/collection/{}'.format(sourceCollection))['name']
+        newCollectionId = getCollectionId(parentCollection, sourceCollectionName)
+
+        if options['bsdType']:
+            newCollection = mbapi.get('/api/collection/{}'.format(newCollectionId))
+            newCollection['name'] = options['bsdType']
+            mbapi.put('/api/collection/{}'.format(newCollectionId), json=newCollection)
+            options['setNames'] = newCollectionId
+            setNames()
+
+        if options['database']:
+            options['collectionId'] = newCollectionId
+            database()
 
 
 def getCollectionId(parentId: int, name: str) -> int:
@@ -58,27 +69,48 @@ def getCollectionId(parentId: int, name: str) -> int:
             return item['id']
 
 
-def parseArguments(args) -> dict:
-    parser = argparse.ArgumentParser(
-        description="Petabase executes mass actions on Metabase cards and dashboards. For now, it's mainly targeted for Trackdéchets. You can find the item id in the URL of the item. E.g. if URL ends with '/21-item-name', the id is 21."
-    )
-    parser.add_argument('--clone', nargs=2, type=int,
-                        help="Clone a card or a collection (arg 1) as a child to an existing collection (arg 2).")
-    parser.add_argument('--bsdType', nargs='?', required=False, default=None,
-                        choices=['BSDD', 'DASRI', 'BSFF', 'VHU', 'BSDA'],
-                        help="The target type of BSD: BSDD, DASRI, BSFF, VHU, BSDA")
-    parser.add_argument('--setNames', nargs='?',
-                        help="Makes sure the name of the cards in a collection (arg 1) contains the type of BSD. Adds it if necessary.")
+def database():
+    collectionId = options['collectionId']
+    newDbName = options['database']
+    items: list = mbapi.get('/api/collection/{}/items'.format(collectionId))['data']
 
-    parsed_args = vars(parser.parse_args())
-    options = {
-        'clone': parsed_args.get('clone'),
-        'bsdType': parsed_args.get('bsdType'),
-        'setNames': parsed_args.get('setNames')
+    db = {
+        'prod': 2,
+        'sandbox': 3
     }
 
-    return options
+    for item in items:
+        if item['model'] == 'card':
+            item['database_id'] = item['database_query']['database'] = db[newDbName]
+            mbapi.put('/api/card/{}'.format(item['id']), json=item)
+
+
+def parseArguments() -> dict:
+    parser = argparse.ArgumentParser(
+        description="Petabase executes mass actions on Metabase cards and dashboards. For now, it's mainly targeted for \
+                    Trackdéchets. You can find the item id in the URL of the item. E.g. if URL ends with '/21-item-name', \
+                    the id is 21."
+    )
+    parser.add_argument('--clone', nargs=2, type=int, default=False,
+                        help="""Clone a card or a collection (arg 1) as a child to an existing collection (arg 2).
+                        Can be used with --database""")
+    parser.add_argument('--database', nargs=1, default=False, choices=['prod', 'sandbox'], required=True,
+                        help="With --clone : set the database of all the clone cards to 'prod' or 'sandbox' (arg 1).")
+    parser.add_argument('--bsdType', nargs=1, required=False, default=False,
+                        choices=['BSDD', 'DASRI', 'BSFF', 'VHU', 'BSDA'],
+                        help="The target type of BSD: BSDD, DASRI, BSFF, VHU, BSDA")
+    parser.add_argument('--setNames', nargs=1, default=False,
+                        help="Makes sure the name of the cards in a collection (arg 1) contains the type of BSD. Adds it if necessary.")
+
+
+    parsed_args = vars(parser.parse_args())
+
+    for arg in parsed_args:
+        if isinstance(parsed_args[arg], list) and len(parsed_args[arg]) == 1:
+            parsed_args[arg] = parsed_args[arg][0]
+
+    return parsed_args
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
