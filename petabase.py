5b33c1cef841306261cb43113ec4b60592399bf0
Colin Maudry <colin@maudry.com>
2021-11-08 16:53:05 +0100

    Added logging

--- petabase.py
+++ petabase.py
@@ -1,7 +1,14 @@
 import argparse
+import datetime
+
 from metabase_api import Metabase_API
 import os
 import re
+import logging
+from datetime import datetime
+
+time = datetime.now().strftime("%Y-%m-%dT%H:%M")
+logging.basicConfig(filename='{}.log'.format(time), encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 mb_url = os.getenv('METABASE_URL')
 mb_user = os.getenv('METABASE_USER')
@@ -10,10 +17,11 @@ mb_password = os.getenv('METABASE_PASSWORD')
 try:
     assert isinstance(mb_url, str) and isinstance(mb_user, str) and isinstance(mb_password, str)
 except AssertionError:
-    print("You must set the environment variables METABASE_URL, METABASE_USER and METABASE_PASSWORD.")
+    logging.error('ERROR: You must set the environment variables METABASE_URL, METABASE_USER and METABASE_PASSWORD.')
     exit(1)
 
 mbapi = Metabase_API(mb_url, mb_user, mb_password)
+logging.info("Metabase API connection established")
 db_schema = 'default$default'
 options = {}
 
@@ -40,16 +48,20 @@ def main():
 def setNames():
     # Set card name and table name in query
 
+    logging.info("Retrieving collection data (%s)...", options['setNames'])
     collection = mbapi.get('/api/collection/{}'.format(options['setNames']))
     bsd_type = collection['name']
+    logging.info("-> Retrieved collection '%s' (%s) for setNames.", bsd_type, options['setNames'])
 
     if not options['bsdType']:
         options['bsdType'] = bsd_type
 
+    logging.debug("Retrieving collection items (%s)...", options['setNames'])
     items: list = mbapi.get('/api/collection/{}/items'.format(options['setNames']))['data']
 
     for item in items:
         if item['model'] == 'card':
+            logging.debug("-> Retrieving card data '%s'...", item['name'])
             card = mbapi.get('/api/card/{}'.format(item['id']))
             if bsd_type not in card['name']:
                 new_name = re.sub(' \([A-Z]+\)$', '', card['name'])
@@ -58,7 +70,7 @@ def setNames():
             card = setTableName(card)
             if options['database']:
                 card = setDatabaseId(card)
-
+            logging.info("-- Pushing card data '%s' with new properties...", card['name'])
             mbapi.put('/api/card/{}'.format(item['id']), json=card)
 
 
@@ -92,16 +104,19 @@ def clone():
     # Equal ids lead to infinite creation of clones
     assert source_collection != parent_collection
 
+    logging.info('Copying collection %s to parent collection %s...', source_collection, parent_collection)
     mbapi.copy_collection(source_collection_id=source_collection,
                           destination_parent_collection_id=parent_collection)
 
     if options['bsdType'] or options['database']:
         source_collection_name = mbapi.get('/api/collection/{}'.format(source_collection))['name']
         new_collection_id = getCollectionId(parent_collection, source_collection_name)
+        logging.info("-> New collection %s created.", new_collection_id)
 
         if options['bsdType']:
             new_collection = mbapi.get('/api/collection/{}'.format(new_collection_id))
             new_collection['name'] = options['bsdType']
+            logging.info("Pushing clone collection %s with new name '%s'...", new_collection['id'], new_collection['name'])
             mbapi.put('/api/collection/{}'.format(new_collection_id), json=new_collection)
             options['setNames'] = new_collection_id
             setNames()
@@ -129,17 +144,22 @@ def getTableId(table_name) -> int:
 
 
 def changeDatabaseInCollection(collection_id):
+    logging.info('Retrieving item list of collection %s for database name change...', collection_id)
     items: list = mbapi.get('/api/collection/{}/items'.format(collection_id))['data']
+    logging.info('-> Items list retrieved (%s items)', len(items))
 
     for item in items:
         if item['model'] == 'card':
             card = mbapi.get('/api/card/{}'.format(item['id']))
             card = setDatabaseId(card)
+            logging.info("-- Pushing card '%s' (%s) with new database id (%s)...", card['name'], card['id'], card['database_id'])
             mbapi.put('/api/card/{}'.format(item['id']), json=card)
+
 
 def setDatabaseId(card) -> dict:
     card['database_id'] = card['dataset_query']['database'] = options['database']
     return card
+
 
 def parseArguments() -> dict:
     parser = argparse.ArgumentParser(
@@ -166,6 +186,7 @@ def parseArguments() -> dict:
             parsed_args[arg] = parsed_args[arg][0]
 
     return parsed_args
+
 
 if __name__ == '__main__':
     main()
